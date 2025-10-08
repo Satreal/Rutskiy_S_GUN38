0380b8b4b499843268bdd3a2515a8985cec9d295
Sergey Rutsky <rutsky.s.v@gmail.com>
2025-10-08 22:25:27 +0300

    add di Zenject

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,16 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    public void OpenMainScene()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void OpenGameScene()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Additive);
+    }
+}
