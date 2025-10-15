050d1357fa9e48baa87cde3fd73618bf107decf0
Sergey Rutsky <rutsky.s.v@gmail.com>
2025-10-15 15:59:11 +0300

    create interface

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Unit : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+    public event Action OnMoveEndCallback;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
